a7022b85f23b837dca9053bffa5c4a3e60032884
Joches29 <jochaveshe@est.utn.ac.cr>
2025-11-08 18:06:47 -0600

    Se crean las excepciones para la clase Carton
    4 excepciones personalizadas para la clase Carton

---
+++ src/Exceptions/Matriz5por5Excepcion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author rodol
+ */
+public class Matriz5por5Excepcion extends Exception{
+
+    public Matriz5por5Excepcion() {
+        super("El carton debe ser 5x5");
+    }
+    
+}
---
+++ src/Exceptions/NumeroInvalidoExcepcion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author rodol
+ */
+public class NumeroInvalidoExcepcion extends Exception {
+
+    public NumeroInvalidoExcepcion() {
+        super("El numero debe estar entre 1 y 75");
+    }
+    
+}
